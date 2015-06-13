0bd3c04adb738c279d9f260519960896a05c5164
Adil Akhter <md.adilakhter@gmail.com>
2015-06-13 22:15:28 +0200

    Extension Method + Pimp My Library pattern

--- src/main/scala/features/implicits/Implicits.scala
+++ src/main/scala/features/implicits/Implicits.scala
@@ -1,8 +1,8 @@
 package features.implicits
 
-object ImplicitsApplication {
+// Example 1: TypeClass
+object TypeClassApp {
 
-  // Example 1: TypeClass
   def lessThan[A: Ordering](a: A, b: A): Boolean = implicitly[Ordering[A]].lt(a, b)
 
   implicit val intOrdering = new Ordering[Int]{
@@ -13,5 +13,20 @@ object ImplicitsApplication {
   val b = 12
 
   val lesser = if(lessThan(a, b)) a else b
+
+
+}
+
+
+// Example 2: Class Extension
+object ClassExtensionApp{
+  implicit class HexableString(s: String) {
+    def asHexVal: Seq[String] = s map { c =>
+      f"0x$c%02x"
+    }
+  }
+
+  // Pimp
+  val haxVal = "implicit".asHexVal
 
 }
