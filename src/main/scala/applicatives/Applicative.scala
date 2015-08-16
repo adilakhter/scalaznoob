7b82043782d1902387efe285a99ffa997ab5638a
Adil Akhter <md.adilakhter@gmail.com>
2015-08-16 21:18:07 +0200

    [wip]: Magnet Pattern, Free Monad and Applicatives

--- src/main/scala/applicatives/Applicative.scala
+++ src/main/scala/applicatives/Applicative.scala
@@ -9,3 +9,88 @@ trait Applicative[A, F[_]]{
 object Applicative {
 
 }
+
+
+object TestApplicative extends App {
+	val f = (x: Int) ⇒ x + 1 
+
+	val g = (x: Int) ⇒ (y: Int) ⇒ x + y 
+
+	val gLifted = Option(5) map g // Option[Int ⇒ Int]
+
+	val h = Option(10)
+}
+
+object WriterMonadEx{
+	trait Monoid[A] {
+		def append(a1: A, a2: A): A
+		def empty: A
+	}
+
+	object Monoid {
+		implicit def ListMonoid[A]: Monoid[List[A]] = new Monoid[List[A]] {
+			def append(a1: List[A], a2: List[A]): List[A] = a1 ::: a2
+			def empty = Nil 
+		}
+	}
+
+	case class Logger[LOG, A](log: LOG, value: A) {
+		def map[B](f: A => B) = Logger(log, f(value))
+		
+		def flatMap[B](f: A => Logger[LOG, B])(implicit m: Monoid[LOG]) = {
+			val x: Logger[LOG, B] = f(value)
+			Logger(m.append(log, x.log), x.value)
+
+		}
+	}
+
+	object Logger {
+		def unital[LOG, A](value: A)(implicit m: Monoid[LOG]) = 
+			Logger(m.empty, value)
+	}
+
+	object Util {
+		implicit def ListLogUtil[A](a: A) = new {
+			def ~>[B](b: B): Logger[List[A], B] = Logger(List(a), b)
+			def <|~[B](k: A => B): Logger[List[B], A] = Logger(List(k(a)), a)
+		}
+
+		def noLog[A](a: A) = 
+			Logger.unital[List[String], A](a)
+	}
+}
+
+object MainWriterMonadEx extends App{
+	import WriterMonadEx._
+	import Util._
+
+	val x = 1 
+	var r = 
+		for(a <- addOne(x);
+				b <- intString(a);
+				c <- lengthIsEven(b);
+				d <- noLog(hundredOrThousand(c));
+				e <- times7(d)
+			) yield e
+
+	println("Result: "+ r.value)
+	println
+	println("LOG")
+	println("---")
+
+	r.log foreach println
+	
+	def addOne(n: Int): Logger[List[String], Int] = ("adding one to"+ n) ~> (n+1)
+
+	def intString(n: Int): Logger[List[String], String] =
+		("converting int to string" + n) ~> n.toString()
+
+	def lengthIsEven(s: String): Logger[List[String], Boolean] =
+		("checking length of "+ s + " for evenness") ~> (s.length % 2 == 0)
+
+	def hundredOrThousand(b: Boolean): Int = // no logging
+    if(b) 100 else 1000
+
+  def times7(n: Int): Logger[List[String], Int] =
+    (n * 7) <|~ ("multiplying " + n + " by 7 to produce " + _) 
+}
--- src/main/scala/patterns/magnet/Magnet.scala
+++ src/main/scala/patterns/magnet/Magnet.scala
@@ -1,7 +1,60 @@
 package patterns.magnet
 
-object MagnetPattern {
 
+/**
+ *  Example taken from
+ *  http://derekwyatt.org/2014/01/11/scala-method-overloading-and-default-argument-values/
+ */
+object MagnetPatternEx1 {
+  // def set (key: String, value: String) (implicit ttl: Duration = 1.hour): Int
+  // def set (key: String, value: ByteString)(implicit ttl: Duration = 2.hour): Int
+
+  // Use magnet pattern to avoid the error raised by compiler:
+  // ... multiple overloaded alternatives of method set define default arguments.
+
+
+  // In this example: return type does not vary with the magnet instance ⇒
+  import akka.util.ByteString
+  import scala.concurrent.duration._
+
+  sealed trait ByteStringMagnet {
+    val bs: ByteString
+    val ttl: Duration
+    def apply(f: (ByteString, Duration) ⇒ Int) : Int = f(bs, ttl)
+  }
+
+  object ByteStringMagnet {
+    implicit def fromString(s: String)(implicit ttlValue: Duration = 1.hour): ByteStringMagnet =
+      new ByteStringMagnet {
+        val ttl = ttlValue
+        val bs = ByteString(s)
+      }
+
+    implicit def fromByteString(bytes: ByteString)(implicit ttlLive: Duration = 2.hours): ByteStringMagnet =
+      new ByteStringMagnet{
+        val ttl = ttlLive
+        val bs = bytes
+      }
+  }
+
+  def set(key: String, magnet: ByteStringMagnet) : Int =
+    magnet { (value, ttl) ⇒
+      ttl.toHours.toInt
+    }
+
+//  Output :
+// =================================================
+//  scala> import patterns.magnet.MagnetPatternEx1._
+//  import patterns.magnet.MagnetPatternEx1._
+//
+//  scala> import akka.util.ByteString
+//  import akka.util.ByteString
+//
+//  scala> set ("key-1", ByteString("test"))
+//  res3: Int = 2
+//
+//  scala> set ("key-2", "test")
+//  res4: Int = 1
 }
 
 
